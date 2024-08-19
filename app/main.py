30ab0324227ff9265f33a5c8e21ea9ca1e15a2f9
Debojit Sinha <sinha.debojit@gmail.com>
2024-08-19 10:23:23 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
 # Uncomment this to pass the first stage
 import socket
 import logging
+import asyncio
 
-def main() -> None:
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, _ = server_socket.accept()
+from asyncio import AbstractEventLoop
 
+async def response(client:socket.socket, loop:AbstractEventLoop):
+    while client:
+        data = await loop.sock_recv(client, 1024)
+        if not data:
+            break
+        await loop.sock_sendall(client, b'+PONG\r\n')
+
+async def handle_connection(server_socket:socket.socket, loop:AbstractEventLoop):
     while True:
-        data:str = client.recv(1024).decode('utf-8')
-        log.debug(f'Received message: {data}')
-        client.send(b'+PONG\r\n')
+        client, addr = await loop.sock_accept(server_socket)
+        client.setblocking(False)
+        asyncio.create_task(response(client, loop))
+
+async def main() -> None:
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.setblocking(False)
+    server_socket.listen()
+    await handle_connection(server_socket, asyncio.get_event_loop())
 
 if __name__ == "__main__":
     # Logger config
     logging.basicConfig(level=logging.DEBUG)
     log = logging.getLogger(__name__)
 
-    main()
+    asyncio.run(main())
